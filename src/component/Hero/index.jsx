2c33e522d4e776b33ef309e17bd906832bb0d839
Den Temple <developer@dentemple.com>
2017-08-05 16:57:40 -0400

    Removed the ForHire component. Needs a better design

--- src/component/Hero/index.jsx
+++ src/component/Hero/index.jsx
@@ -8,12 +8,10 @@ import GithubRibbon from './GithubRibbon'
 import MyName from './MyName'
 import MyTagline from './MyTagline'
 import HorizontalLine from './HorizontalLine'
-import ForHire from './ForHire'
 
 const Hero = ({ forHire }: { forHire: boolean }) =>
   <Container>
     <GithubRibbon />
-    <ForHire forHire={forHire} />
     <MyName />
     <HorizontalLine />
     <MyTagline />
